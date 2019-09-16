cf7d18d9b3eb03b6ce8d7f5c09c8aa14fe0a69fc
catherineuhs <cliffordcat@urbandaleschools.com>
2019-09-16 17:11:33 -0500

    temperature

--- 3.2_Trapezoid.py
+++ 3.2_Trapezoid.py
@@ -10,3 +10,11 @@ base 1: 5       base 2: 7    height: 2    area: 12
 base 1: 1       base 2: 2    height: 3    area: 4.5
 base 1: 7       base 2: 2    height: 4    area: 18
 '''
+
+baseA=int(input("Whats the top base of the trapezoid?"))
+baseB=int(input("Whats the bottom base of the trapezoid?"))
+height=int(input("Whats the height of the trapezoid?"))
+area=((baseA+baseB)/2 * height)
+print("the area of the trapezoid is", area)
+
+
--- 3.3_Grading.py
+++ 3.3_Grading.py
@@ -12,3 +12,12 @@ Sem Grade: 86   Final Exam: 52   Exam worth: 15%    Overall: 80.9
 Sem Grade: 95   Final Exam: 32   Exam worth: 10%    Overall: 88.7
 Sem Grade: 72   Final Exam: 100   Exam worth: 20%    Overall: 77.6
 '''
+
+
+semester_grade=float(input("whats your semester grade?"))
+final_exam_grade=float(input("whats your final exam grade?"))
+final_exam_worth=float(input("whats your final exam worth?"))
+final_exam_worth=final_exam_worth/100
+semester_grade_worth=1-final_exam_worth
+avg=semester_grade*semester_grade_worth+final_exam_grade*final_exam_worth
+print(avg)
